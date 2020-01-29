f4dc889dbbb00eb9789b6b7fe4099d013999f259
tornhill007 <tornhill1997@gmail.com>
2020-01-29 20:08:53 +0300

    test

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://forklog.com/wp-content/uploads/social-network1.jpg" alt="emblem"/>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://techcrunch.com/wp-content/uploads/2017/05/32786886766_0b0b236911_o.jpg"
+                     alt="wallpaper"/>
+            </div>
+            <div>
+                ava + desc
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
